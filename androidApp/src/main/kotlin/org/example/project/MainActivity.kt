3aa02dfa6e91bccce43c87bdfb2a57bc61edfe7b
Vegardmykle <gass_genera_04@icloud.com>
2026-07-07 18:34:59 +0200

    add db and rep to MainActivity.kt

--- androidApp/src/main/kotlin/org/example/project/MainActivity.kt
+++ androidApp/src/main/kotlin/org/example/project/MainActivity.kt
@@ -4,22 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import org.example.project.data.ChecklistRepository
+import org.example.project.data.createDriver
+import org.example.project.db.AppDatabase
+import org.example.project.storage.AndroidDocumentStorage
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        val database = AppDatabase(createDriver(applicationContext))
+        val repository = ChecklistRepository(database)
+        val documentStorage = AndroidDocumentStorage(applicationContext)
+
         setContent {
-            App()
+            App(
+                repository = repository,
+                documentStorage = documentStorage,
+            )
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }

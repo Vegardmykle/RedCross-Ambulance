9f307034eca72673317b64e3a4e8b5e9da7da9d2
Vegardmykle <gass_genera_04@icloud.com>
2026-08-11 09:43:32 +0200

    rm unused imp

--- sharedUI/src/androidMain/kotlin/org/example/project/ui/ResourcesScreen.kt
+++ sharedUI/src/androidMain/kotlin/org/example/project/ui/ResourcesScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Apps
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Edit
